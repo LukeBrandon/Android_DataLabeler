cfda6fed3a0647e14dc2aab86c679df8a7a9d0a1
samuelle <samle14460@gmail.com>
2019-10-23 20:03:52 -0500

    Added a setting button to the main activity, a reward activity, settings activity, and views corresponding to the activities

---
+++ app/src/main/java/dev/threepebbles/datalabeler/view/RewardActivity.java
@@ -0,0 +1,15 @@
+package dev.threepebbles.datalabeler.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dev.threepebbles.datalabeler.R;
+
+public class RewardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedBundleInstance) {
+        super.onCreate(savedBundleInstance);
+        setContentView(R.layout.reward_layout);
+    }
+}
