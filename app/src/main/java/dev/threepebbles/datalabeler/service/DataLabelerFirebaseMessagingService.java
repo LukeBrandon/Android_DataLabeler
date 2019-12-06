c4d6b17167219fa306e5904d84be67900f595723
LukeBrandon <lukebrandon68@gmail.com>
2019-12-06 12:49:09 -0600

    got rid of useless code

--- app/src/main/java/dev/threepebbles/datalabeler/service/DataLabelerFirebaseMessagingService.java
+++ app/src/main/java/dev/threepebbles/datalabeler/service/DataLabelerFirebaseMessagingService.java
@@ -24,26 +24,7 @@ public class DataLabelerFirebaseMessagingService extends FirebaseMessagingServic
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            scheduleNotification();
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
     }
 
-
-    private void scheduleNotification(){
-
-    }
 }
